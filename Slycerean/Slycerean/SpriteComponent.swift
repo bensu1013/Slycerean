4d29ef2b163ac9e8289adb5a1750bf3bcd9dc4b4
Benjamin Su <bensu1013@hotmail.com>
2017-10-23 21:41:56 -0400

    spritesheet added

--- Slycerean/Slycerean/SpriteComponent.swift
+++ Slycerean/Slycerean/SpriteComponent.swift
@@ -27,12 +27,12 @@ let spriteTexture = SKTexture.init(imageNamed: "Character_Hero_Warrior")
 
 class SpriteComponent: SKSpriteNode {
     weak var unit: GameUnit?
+    var spriteSheet: SpriteSheet
     var direction: Direction = .down
     init(unit: GameUnit) {
+        spriteSheet = SpriteSheet(texture: spriteTexture, columns: 6, rows: 3)
         super.init(texture: nil, color: .clear, size: CGSize(width: 128.0, height: 128.0))
         self.name = "Unit"
-//        self.texture = SKTexture.init(rect: CGRect.init(x: 0, y: 200, width: 100, height: 100), in: spriteTexture)
-        
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -42,56 +42,32 @@ class SpriteComponent: SKSpriteNode {
     func walk(_ direction: Direction, withKey: String) {
         if self.direction != direction {
             self.direction = direction
-            let x: CGFloat = 0
-            var y: CGFloat
+            removeAllActions()
             switch direction {
             case .down:
-                y = 0
+                let t1 = spriteSheet.getTextureFrom(col: 2, row: 0)
+                let t2 = spriteSheet.getTextureFrom(col: 3, row: 0)
+                run(SKAction.repeatForever(SKAction.animate(with: [t1, t2], timePerFrame: 0.5)))
             case .up:
-                y = 1
+                let t1 = spriteSheet.getTextureFrom(col: 2, row: 1)
+                let t2 = spriteSheet.getTextureFrom(col: 3, row: 1)
+                run(SKAction.repeatForever(SKAction.animate(with: [t1, t2], timePerFrame: 0.5)))
             case .left:
-                y = 2
-//                xScale = xScale < 0 ? xScale * -1 : xScale
+                let t1 = spriteSheet.getTextureFrom(col: 2, row: 2)
+                let t2 = spriteSheet.getTextureFrom(col: 3, row: 2)
+                anchorPoint = CGPoint(x: 0, y: 0)
+                run(SKAction.scaleX(to: 1, duration: 0))
+                run(SKAction.repeatForever(SKAction.animate(with: [t1, t2], timePerFrame: 0.5)))
             case .right:
-                y = 2
-//                xScale = xScale > 0 ? xScale * -1 : xScale
+                let t1 = spriteSheet.getTextureFrom(col: 2, row: 2)
+                let t2 = spriteSheet.getTextureFrom(col: 3, row: 2)
+                anchorPoint = CGPoint(x: 1, y: 0)
+                run(SKAction.scaleX(to: -1, duration: 0))
+                run(SKAction.repeatForever(SKAction.animate(with: [t1, t2], timePerFrame: 0.5)))
             }
 
-//            let t2 = SKTexture.init(rect: CGRect.init(x: spriteTexture.textureRect().maxX/(x+1),
-//                                                      y: spriteTexture.textureRect().maxY/y,
-//                                                      width: 100/spriteTexture.size().width,
-//                                                      height: 100/spriteTexture.size().height), in: spriteTexture)
-            
-            removeAllActions()
-            var textureRect = CGRect(x: x*(spriteTexture.size().width/6),
-                                     y: y*(spriteTexture.size().height/3),
-                                     width: spriteTexture.size().width/6,
-                                     height: spriteTexture.size().height/3)
-            
-            textureRect = CGRect(x: textureRect.origin.x/spriteTexture.size().width,
-                                 y: textureRect.origin.y/spriteTexture.size().height,
-                                 width: textureRect.size.width/spriteTexture.size().width,
-                                 height: textureRect.size.height/spriteTexture.size().height)
-            
-            let t1 = SKTexture(rect: textureRect, in: spriteTexture)
-            
-            textureRect = CGRect(x: (x+1)*(spriteTexture.size().width/6),
-                                 y: y*(spriteTexture.size().height/3),
-                                 width: spriteTexture.size().width/6,
-                                 height: spriteTexture.size().height/3)
-            
-            textureRect = CGRect(x: textureRect.origin.x/spriteTexture.size().width,
-                                 y: textureRect.origin.y/spriteTexture.size().height,
-                                 width: textureRect.size.width/spriteTexture.size().width,
-                                 height: textureRect.size.height/spriteTexture.size().height)
-            
-            let t2 = SKTexture(rect: textureRect, in: spriteTexture)
-            
-            run(SKAction.repeatForever(SKAction.animate(with: [t1, t2], timePerFrame: 0.5)))
         }
-        //runAnimation(with: spriteTextures[direction.rawValue]!, andKey: withKey)
     }
-    
     
     private func runAnimation(with textures: [SKTexture], andKey key: String) {
         let animate = SKAction.animate(with: textures, timePerFrame: 0.2)
